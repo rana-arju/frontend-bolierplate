9b1ddfafc61be344290cf62dda09a1a7e8f67b36
rana-arju <arjurana20@gmail.com>
2025-10-11 16:30:36 +0600

    fix bug

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to the Home Page  </h1>
+      <h1 className="text-4xl font-bold">Welcome! You are pass CI/CD practice!  </h1>
       
     </main>
   );
